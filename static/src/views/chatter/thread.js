3c98e9c26386462112cffd9c31778d65994b2f9a
WS Loh <wsloh@perfectwork.com>
2026-08-23 15:49:17 +0800

    fix(chatter): remove invalid attachment preview xpath and use prototype getter for thread messages

--- static/src/views/chatter/thread.js
+++ static/src/views/chatter/thread.js
@@ -6,17 +6,12 @@
 */
 
 import { patch } from "@web/core/utils/patch";
-
 import { Thread } from "@mail/core/common/thread";
 
 patch(Thread.prototype, {
-    get displayMessages() {
-        let messages = (
-            this.props.order === "asc" ?
-            this.props.thread.nonEmptyMessages :
-            [...this.props.thread.nonEmptyMessages].reverse()
-        );
-        if (!this.props.showNotificationMessages) {
+    get orderedMessages() {
+        let messages = super.orderedMessages;
+        if (this.props.showNotificationMessages === false && messages) {
             messages = messages.filter(
                 (msg) => !["user_notification", "notification"].includes(
                     msg.message_type
